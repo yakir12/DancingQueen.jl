cecedeab8fe089fe8983f9678124690f7a24cef6
Yakir Gagnon <12.yakir@gmail.com>
2022-05-05 19:37:09 +0200

    rm the videowriter

--- src/logs.jl
+++ src/logs.jl
@@ -1,7 +1,7 @@
 struct Log
 
   csvio::IOStream
-  videoio::VideoIO.VideoWriter
+  # videoio::VideoIO.VideoWriter
   csvof::Observables.ObserverFunction
   videoof::Observables.ObserverFunction
 
@@ -12,9 +12,9 @@ struct Log
     csvio = open(string(dt, ".csv"), "w")
     println(csvio, "datetime,p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,red,green,blue,width,azimuth,red_σ,green_σ,blue_σ,width_σ,azimuth_σ,a,b")
 
-    framerate = 5
-    encoder_options = (crf=23, preset="medium")
-    videoio = open_video_out(string(dt, ".mp4"), rotl90(frame[]); framerate, encoder_options)
+    # framerate = 5
+    # encoder_options = (crf=23, preset="medium")
+    # videoio = open_video_out(string(dt, ".mp4"), rotl90(frame[]); framerate, encoder_options)
 
     csvof = on(row) do r
       println(csvio, now(), ",", join(r, ","))
@@ -37,7 +37,7 @@ struct Log
     #   end
     # end
 
-    new(csvio, videoio, csvof, videoof)
+    new(csvio, csvof, videoof)
   end
 end
 
@@ -45,7 +45,7 @@ function Base.close(l::Log)
   off(l.csvof)
   close(l.csvio)
   off(l.videoof)
-  close_video_out!(l.videoio)
+  # close_video_out!(l.videoio)
 end
 
 Base.close(::Nothing) = nothing
